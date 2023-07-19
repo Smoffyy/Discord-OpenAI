ce0022a691fbc3b58d99024a3c956767aa7af2a5
Smoffyy <kidcapcom08@gmail.com>
2023-07-19 17:33:15 -0500

    added start

---
+++ start.bat
@@ -0,0 +1,6 @@
+@echo off
+echo Starting.. Bot
+:main
+node .
+echo Looks like the bot crashed, restarting the bot.
+goto main
---
+++ start.sh
@@ -0,0 +1,7 @@
+# /bin/sh
+while true; do
+    echo "Starting Bot ..."
+    # The command to start your bot here
+    echo "Bot crashed ... Restarting in 5 seconds..."
+    sleep 5
+done
